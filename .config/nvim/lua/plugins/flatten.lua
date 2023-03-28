60b364baa595102723d454671cc59183384a42f8
Roman Komkov <roman.komkov@workday.com>
2023-03-29 01:07:52 +0200

    [neovim] flatten.nvim replaces unception

---
+++ .config/nvim/lua/plugins/flatten.lua
@@ -0,0 +1,32 @@
+return {
+  "willothy/flatten.nvim",
+  opts = {
+    window = {
+      open = "current",
+    },
+    callbacks = {
+      pre_open = function()
+        require("toggleterm").toggle(0)
+      end,
+      post_open = function(bufnr, winnr, ft)
+        if ft == "gitcommit" then
+          vim.api.nvim_create_autocmd("BufWritePost", {
+            buffer = bufnr,
+            once = true,
+            callback = function()
+              vim.defer_fn(function()
+                vim.api.nvim_buf_delete(bufnr, {})
+              end, 50)
+            end,
+          })
+        else
+          require("toggleterm").toggle(0)
+          vim.api.nvim_set_current_win(winnr)
+        end
+      end,
+      block_end = function()
+        require("toggleterm").toggle(0)
+      end,
+    },
+  },
+}
