a97823452a3b7c9bae487e84d1e2f81d54cf0f85
Roman Komkov <roman.komkov@workday.com>
2024-02-03 14:50:49 +0100

    [neovim] neo-tree upgrade

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,20 +1,27 @@
 local M = {
   "nvim-neo-tree/neo-tree.nvim",
-  branch = "v2.x",
-  cmd = { "Neotree", "NeoTreeRevealToggle" },
+  branch = "v3.x",
+  cmd = { "Neotree" },
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "kyazdani42/nvim-web-devicons", lazy = true }, -- not strictly required, but recommended
-    { "MunifTanjim/nui.nvim", lazy = true },
+    { "MunifTanjim/nui.nvim",         lazy = true },
+  },
+  keys = {
+    { "<leader>F", "<cmd>Neotree reveal toggle<cr>", desc = "Toggle Filetree" },
   },
 }
 
 function M.config()
   require("neo-tree").setup({
+    window = {
+      position = "right",
+    },
     sync_root_with_cwd = true,
     respect_buf_cwd = true,
     filesystem = {
-      follow_current_file = true,
+      follow_current_file = {
+        enabled = true, },
       hijack_netrw_behavior = "open_current",
       use_libuv_file_watcher = true,
     },
